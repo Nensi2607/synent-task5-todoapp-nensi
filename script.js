a97c195b5c2d578b060fdd0384d26563d66d7e37
Nensi2607 <nensishingala409@gmail.com>
2026-05-24 13:33:52 +0530

    Added local storage support

--- script.js
+++ script.js
@@ -2,43 +2,53 @@ const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 
+/* Load Tasks When Page Opens */
+
+window.addEventListener("DOMContentLoaded", loadTasks);
+
 /* Add Task Function */
 
 function addTask(){
 
     const taskText = taskInput.value.trim();
 
-    /* Input Validation */
-
     if(taskText === ""){
         alert("Please enter a task!");
         return;
     }
 
-    /* Create Task Item */
+    createTaskElement(taskText, false);
+
+    saveTasks();
+
+    taskInput.value = "";
+}
+
+/* Create Task Element */
+
+function createTaskElement(taskText, completed){
 
     const li = document.createElement("li");
 
     li.classList.add("task-item");
 
+    if(completed){
+        li.classList.add("completed");
+    }
+
     li.innerHTML = `
-    <div class="task-content">
+        <div class="task-content">
 
-        <input type="checkbox" class="check-task">
+            <input type="checkbox" class="check-task" ${completed ? "checked" : ""}>
 
-        <span>${taskText}</span>
+            <span>${taskText}</span>
 
-    </div>
+        </div>
 
-    <button class="delete-btn">Delete</button>
-`;
-    /* Add Task to List */
+        <button class="delete-btn">Delete</button>
+    `;
 
     taskList.appendChild(li);
-
-    /* Clear Input */
-
-    taskInput.value = "";
 
     /* Delete Task */
 
@@ -48,21 +58,64 @@ function addTask(){
 
         li.remove();
 
+        saveTasks();
+
     });
 
     /* Complete Task */
 
-const checkTask = li.querySelector(".check-task");
+    const checkTask = li.querySelector(".check-task");
 
-checkTask.addEventListener("change", function(){
+    checkTask.addEventListener("change", function(){
 
-    li.classList.toggle("completed");
+        li.classList.toggle("completed");
 
-});
+        saveTasks();
+
+    });
 
 }
 
-/* Add Button Click */
+/* Save Tasks */
+
+function saveTasks(){
+
+    const tasks = [];
+
+    const allTasks = document.querySelectorAll(".task-item");
+
+    allTasks.forEach(function(task){
+
+        const text = task.querySelector("span").innerText;
+
+        const completed = task.classList.contains("completed");
+
+        tasks.push({
+            text,
+            completed
+        });
+
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+}
+
+/* Load Tasks */
+
+function loadTasks(){
+
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    storedTasks.forEach(function(task){
+
+        createTaskElement(task.text, task.completed);
+
+    });
+
+}
+
+/* Add Button */
 
 addBtn.addEventListener("click", addTask);
 
